6e57d787568cdd73e6d3e4dc490a11b8a9514e04
Kimberly Koh <13kkoh@gmail.com>
2023-09-07 23:30:59 -0500

    still getting an error for a missing key prop even though there is clearly on there

--- src/app/Components/SetOfQuestions.jsx
+++ src/app/Components/SetOfQuestions.jsx
@@ -18,8 +18,8 @@ function SetOfQuestions(props) {
 
     return(<div className={styles.questionsContainer}>
         {triviaQuestions.length === 0 ? <p>loading</p> : triviaQuestions.map((question, i) => 
-                (<Question
-                    key = {question.question + question.correctAnswer}
+                (<div key = {question.question + question.correctAnswer}>
+                <Question
                     number = {i+1}
                     type = {question.type}
                     question = {question.question}
@@ -28,7 +28,7 @@ function SetOfQuestions(props) {
                     gradeAnswer = {props.gradeAnswer}
                     submitted = {props.submitted}
                     answerPoints = {props.answerPoints}
-                />)
+                /></div>)
         )}
     </div>)
 }
